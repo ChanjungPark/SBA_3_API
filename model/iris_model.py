1160247e22a25c09145c950752ca7f68ee6bcb4a
ChanjungPark <chan09062000@naver.com>
2020-10-05 16:34:09 +0900

    commit

--- model/iris_model.py
+++ model/iris_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from model.perceptron import Perceptron
 
 class IrisModel:
     def __init__(self):
@@ -19,6 +21,19 @@ class IrisModel:
         # Iris-setosa 와 versicolor 선택 (MVC 는 이진분류만 할 수 있다)
         t = self.iris.iloc[0:100]
         self.y = np.where(t, 'Iris-setosa', -1, 1)
-
         # 꽃받침 길이, 꽃잎 추출
-        self.x = self.iris.iloc[0:100, [0,2].value]
+        self.x = self.iris.iloc[0:100, [0,2]].values
+        self.clf = Perceptron(eta=0.1, n_iter=10)
+
+    def get_iris(self):
+        return self.iris
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def draw_scatter(self):
+        X = self.x
+        
